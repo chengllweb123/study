b25432a908fd0bf81fe8bfade4feae1c7db8451d
“chengllweb123” <cllyl721521@163.com>
2021-01-15 12:00:36 +0800

    退出功能

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -11,6 +11,7 @@
                 <div class="topbar-user">
                     <a href="javascript:;" v-if="username">{{username}}</a>
                     <a href="javascript:;" v-if="!username" @click="login">登录</a>
+                    <a href="javascript:;" v-if="username" @click="logout">退出</a>
                     <a href="javascript:;" v-if="username">我的订单</a>
                     <a href="javascript:;" class="my-cart"  @click="toCart"><span class="icon-cart"></span>购物车({{cartCount}})</a>
                 </div>
@@ -83,10 +84,30 @@
         },
         mounted(){
             this.getProductList();
+            let params=this.$route.params;
+            if(params && params.from=='login'){
+                this.getCartCount();
+            }
+           
         },
         methods:{
             login(){
                 this.$router.push('/login');
+            },
+            logout(){
+                this.axios.post('/user/logout').then(()=>{
+                    this.$message.warning('退出成功');
+                    this.$cookie.set('userId',"",{expires:"-1"});
+                    this.$store.dispatch("saveUserName",'');
+                    this.$store.dispatch("saveCartCount",'0');
+                   
+                  
+                });
+            },
+            getCartCount(){
+                    this.axios.get("/carts/products/sum").then((res=0)=>{
+                        this.$store.dispatch("saveCartCount",res);
+                    })
             },
             toCart(){
                 console.log(123);
